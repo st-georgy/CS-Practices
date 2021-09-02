841661b0b9843ad84a0d26d33853c1c3841e6e65
st_georgy <zhidkov.georg@gmail.com>
2021-09-02 18:17:25 +0300

    Добавьте файлы проекта.

---
+++ Practice 1/task-1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace task_1
+{
+    class Program
+    {
+        public enum Genders : int { Male, Female };
+
+        struct Person
+        {
+            public string firstName;
+            public string lastName;
+            public int age;
+            public Genders gender;
+
+            public Person(string _firstName, string _lastName, int _age, Genders _gender)
+            {
+                firstName = _firstName;
+                lastName = _lastName;
+                age = _age;
+                gender = _gender;
+            }
+
+            public override string ToString()
+            {
+                return $"{firstName} {lastName} ({gender.ToString()[0]}), {age}";
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Person p = new Person("Georgy", "Zhidkov", 18, Genders.Male);
+            Console.WriteLine(p);
+            Console.ReadKey();
+        }
+    }
+}
