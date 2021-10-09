50206c898449c49956682ad25b6a39534a145b52
st_georgy <zhidkov.georg@gmail.com>
2021-10-09 14:35:03 +0300

    Full Practice 3

---
+++ Practice 3/task-1-2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace task_1_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ Practice 3/task-2/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.IO;
+
+namespace task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader reader = new StreamReader(@"boot.ini");
+            StreamWriter writer = new StreamWriter(@"boot-utf7.txt", false, System.Text.Encoding.UTF7);
+
+            writer.WriteLine(reader.ReadToEnd());
+            
+            writer.Close();
+            reader.Close();
+        }
+    }
+}
